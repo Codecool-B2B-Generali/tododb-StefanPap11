846268f78e5766a0e44c2e14f71d93c53b8abb1e
Stefan <89575526+StefanPap11@users.noreply.github.com>
2021-09-09 11:37:18 +0200

    Update task-4.sql
    add

--- task-4.sql
+++ task-4.sql
@@ -1,1 +1,17 @@
 -- Query all tasks that are done
+USE [tododb]
+GO
+
+INSERT INTO [dbo].[users]
+           ([id]
+           ,[name])
+     VALUES
+           (1,'Jane'),
+           (2,'John'),
+           (3,'Dave'),
+           (4,'Emma'),
+           (5,'Robert');
+
+
+
+GO
