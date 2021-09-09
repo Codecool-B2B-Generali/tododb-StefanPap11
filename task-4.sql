dbd6cd27015886f03a20279542c94022504f013a
Stefan <89575526+StefanPap11@users.noreply.github.com>
2021-09-09 11:47:17 +0200

    Update task-4.sql
    add

--- task-4.sql
+++ task-4.sql
@@ -15,3 +15,29 @@ INSERT INTO [dbo].[users]
 
 
 GO
+USE [tododb]
+GO
+
+INSERT INTO [dbo].[todo]
+           ([ID]
+           ,[task]
+           ,[user_id]
+           ,[done])
+     VALUES
+			(1,'Setup pgAdmin ',2,0),
+			(2,'Download Git  ',2,0),
+			(3,'Setup VS Code  ',1,1),
+			(4,'Download  PostgreSQL',2,0),
+			(5,'Install server ',2,0),
+			(6,'Create superuser   ',2,0),
+			(7,'Create database  ',2,0),
+			(8,'Create tables  ',2,0),
+			(9,'Wash the dishes ',3,0),
+			(10,'Read the PostgreSQL manual',4,0),
+			(11,'Exercise    ',3,0),
+			(12,'Wake up in time!   ',5,1),
+			(13,' Go to the gym   ',3,0),
+			(14,'Wash the dishes',5,1);
+
+GO
+
