91c49276ab414e418e0a939886c31a11bb7d73af
Stefan <89575526+StefanPap11@users.noreply.github.com>
2021-09-09 10:22:37 +0200

    Update task-1.sql

--- task-1.sql
+++ task-1.sql
@@ -1,1 +1,17 @@
 -- Create two tables, one to store the users and one for the todos
+
+
+    CREATE TABLE users (
+    id INT ,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id ),
+    );
+
+
+	CREATE TABLE todo (
+	ID int NOT NULL,
+    task VARCHAR(100) NOT NULL,
+    user_id int NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (user_id) REFERENCES users(ID)
+    );
