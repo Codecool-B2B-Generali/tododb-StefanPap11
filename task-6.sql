bbd53ea0a604d90370364b4d5643eb4d86b7ff36
Stefan <89575526+StefanPap11@users.noreply.github.com>
2021-09-09 12:11:28 +0200

    Update task-6.sql
    done

--- task-6.sql
+++ task-6.sql
@@ -1,1 +1,10 @@
 -- Delete tasks from the todo table that are done already
+delete from todo where   done = 1 ;
+
+select * from   todo 
+
+
+9	  Wash the dishes 	3	0
+10	Read the PostgreSQL manual	4	0
+11	Exercise    	3	0
+13	 Go to the gym   	3	0
