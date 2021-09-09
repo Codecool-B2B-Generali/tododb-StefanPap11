379699f6e2b191825fca73e07b68aaed14879ca3
Stefan <89575526+StefanPap11@users.noreply.github.com>
2021-09-09 12:05:59 +0200

    Update task-5.sql
    done

--- task-5.sql
+++ task-5.sql
@@ -1,1 +1,16 @@
 -- Update John's tasks and mark them as done
+select * from  users a INNER JOIN todo b on a.id= b.user_id
+where  a.id =2;
+
+update todo set done = 1 from  users a INNER JOIN todo b on a.id= b.user_id
+where  a.id =2;
+
+
+
+2	John	1	Setup pgAdmin 	2	1
+2	John	2	Download Git  	2	1
+2	John	4	Download  PostgreSQL	2	1
+2	John	5	Install server 	2	1
+2	John	6	Create superuser   	2	1
+2	John	7	Create database  	2	1
+2	John	8	Create tables  	2	1
